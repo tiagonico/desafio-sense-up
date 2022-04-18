99c3a915d044d139fdd8df29efe9ae7a885ea14b
Tiago Silva <tiagosilva0512@gmail.com>
2022-04-18 17:59:42 -0300

    Primeiro commit

---
+++ src/main/java/com/tiago/desafiosenseup/DesafioSenseUpApplication.java
@@ -0,0 +1,13 @@
+package com.tiago.desafiosenseup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSenseUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSenseUpApplication.class, args);
+	}
+
+}
